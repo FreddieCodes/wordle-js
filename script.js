965f5d9072dc763446aabab43f0906ba09232d11
FreddieCodes <FrederickCodes@gmail.com>
2022-02-13 20:22:28 +0000

    User Interaction js

--- script.js
+++ script.js
@@ -15290,3 +15290,51 @@ const dictionary = [
   "shave"
 ];
 
+startInteraction();
+
+function startInteraction() {
+  document.addEventListener("click", handleMouseClick);
+  document.addEventListener("keydown", handleKeyPress);
+}
+
+function stopInteraction() {
+  document.removeEventListener("click", handleMouseClick);
+  document.removeEventListener("keydown", handleKeyPress);
+}
+
+function handleMouseClick(e) {
+  if (e.target.matches("[data-key]")) {
+    pressKey(e.target.dataset.key);
+    return;
+  }
+
+  if (e.target.matches("[data-enter]")) {
+    submitGuess();
+    return;
+  }
+
+  if (e.target.matches("[data-delete]")) {
+    deleteKey();
+    return;
+  }
+  
+}
+
+function handleKeyPress(e) {
+  console.log(e);
+  if(e.key === "Enter" ) {
+    submitGuess();
+    return;
+  }
+
+  if(e.key === "Backspace" || e.key === "Delete" ) {
+    deleteKey();
+    return;
+  }
+
+  if(e.key.match(/[a-z]$/) ) {
+    pressKey(e.key);
+  }
+
+}
+
